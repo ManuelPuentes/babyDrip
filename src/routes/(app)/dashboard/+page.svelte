5a062ea86dde6b292217f0483c69b0ec5a7395ea
ManuelPuentes <manuel.r.puentes.d@gmail.com>
2025-04-25 20:32:09 -0400

    infinite loading style added in dsashboard

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,33 +1,51 @@
 <script lang="ts">
 	import { getProductsPaginatedData } from '$lib/api/getProductsPaginated.api.js';
+	import { onMount } from 'svelte';
 
 	export let data;
-	let { supabase, maxPageNumber, products } = data;
+	let { supabase, maxPageNumber } = data;
 
+	let products: {
+		id: any;
+		description: any;
+		size: any;
+		sold_price: any;
+		cost: any;
+	}[] = [];
+
+	let load_trigger: HTMLElement;
+	let loading: boolean = false;
 	let pageNumber = 0;
 
-	const nextPage = () => {
-		if (pageNumber + 1 < maxPageNumber) pageNumber++;
-	};
-	const prevPage = () => {
-		if (pageNumber > 0) pageNumber--;
-	};
-
-	$: fetchProductsData(pageNumber);
-
-	const fetchProductsData = async (pageNumber: number) => {
-		const { products: _products } = await getProductsPaginatedData(pageNumber, supabase);
-		products = _products;
-	};
+	onMount(async (): Promise<any> => {
+		const observer = new IntersectionObserver(
+			async (entries) => {
+				if (loading || pageNumber >= maxPageNumber) return;
+				loading = true;
+				const { products: _products } = await getProductsPaginatedData(pageNumber, supabase);
+				products.push(..._products);
+				products = products;
+				pageNumber++;
+				loading = false;
+			},
+			{
+				threshold: 0.5
+			}
+		);
+		observer.observe(load_trigger);
+		return () => {
+			observer.unobserve(load_trigger);
+		};
+	});
 </script>
 
 <div
-	class="flex max-w-[700px] flex-col items-center p-4 select-none lg:m-auto lg:min-h-1/2 lg:w-1/2"
+	class="m-auto flex max-w-[700px] flex-col items-center overflow-y-scroll p-4 select-none md:max-h-1/3 lg:min-h-1/2 lg:w-1/2"
 >
 	<h1 class=" text-center text-2xl font-semibold">Sistema de Inventario</h1>
 
 	<div>
-		<table class="table-xs table-pin-rows table-pin-cols m-5">
+		<table class="table-xs table-pin-rows table-pin-cols table-zebra m-5">
 			<thead class="border-b">
 				<tr>
 					<td>ID</td>
@@ -39,8 +57,8 @@
 			</thead>
 
 			<tbody>
-				{#each products as product, index (product.id)}
-					<tr class="hover:bg-base-300 cursor-pointer">
+				{#each products as product, index}
+					<tr class="cursor-pointer hover:bg-zinc-100">
 						<td>{index + 1}</td>
 						<td>{product.description}</td>
 						<td>{product.size}</td>
@@ -52,9 +70,9 @@
 		</table>
 	</div>
 
-	<div class="join flex justify-center">
-		<button class="join-item btn" onclick={prevPage}>«</button>
-		<button class="join-item btn">{pageNumber + 1}</button>
-		<button class="join-item btn" onclick={nextPage}>»</button>
-	</div>
+	<div id="load-more-trigger" style="height: 20px;" bind:this={load_trigger}></div>
+
+	{#if loading}
+		<span class="loading loading-spinner loading-xs">aaa</span>
+	{/if}
 </div>
